4bc16e56d44ef2d4122a08df61c8bdeb7fc7e3ce
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-23 14:30:04 -0400

    use sync.WRMutex and add print line  into operations

--- sync/main.go
+++ sync/main.go
@@ -9,26 +9,36 @@ var (
 	balance int = 100
 )
 
-func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
+func Deposit(amount int, wg *sync.WaitGroup, lock *sync.RWMutex) {
 	defer wg.Done()
 	lock.Lock()
+	fmt.Println("Depositing:", amount)
 	b := balance
 	balance = b + amount
 	lock.Unlock()
 }
 
-func Balance() int {
+func Balance(lock *sync.RWMutex) int {
+	lock.RLock()
 	b := balance
+	lock.RUnlock()
 	return b
+}
+
+func ShowBalance(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Balance:", balance)
 }
 
 func main() {
 	var wg sync.WaitGroup
-	var lock sync.Mutex
+	var lock sync.RWMutex
 	for i := 1; i <= 5; i++ {
-		wg.Add(1)
+		wg.Add(2)
 		go Deposit(i*100, &wg, &lock)
+		go ShowBalance(&wg)
 	}
 	wg.Wait()
-	fmt.Println("Balance:", Balance())
+	fmt.Println("------------------------")
+	fmt.Println("Final balance:", Balance(&lock))
 }
