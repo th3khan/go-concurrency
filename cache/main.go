f925215527ca98518f850145008e7c09e1df9c4c
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-23 18:24:20 -0400

    add concurrency to system cache

--- cache/main.go
+++ cache/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -17,6 +18,7 @@ func Fibonacci(n int) int {
 type Memory struct {
 	f     Function               // Function to be used
 	cache map[int]FunctionResult // Map of results for a given key
+	lock  sync.Mutex             // Mutex to protect the cache edit
 }
 
 // A function has to recive a value and return a value and an error
@@ -30,19 +32,23 @@ type FunctionResult struct {
 
 // NewCache creates a new cache
 func NewCache(f Function) *Memory {
-	return &Memory{f, make(map[int]FunctionResult)}
+	return &Memory{f, make(map[int]FunctionResult), sync.Mutex{}}
 }
 
 // Get returns the value for a given key
 func (m *Memory) Get(key int) (interface{}, error) {
-
+	// Block until the cache is verified
+	m.lock.Lock()
 	// Check if the value is in the cache
 	res, exist := m.cache[key]
-
+	m.lock.Unlock()
 	// If the value is not in the cache, calculate it
 	if !exist {
+		// Block until the cache is edited
+		m.lock.Lock()
 		res.value, res.err = m.f(key) // Calculate the value
 		m.cache[key] = res            // Store the value in the cache
+		m.lock.Unlock()
 	}
 	return res.value, res.err
 }
@@ -57,14 +63,23 @@ func main() {
 	cache := NewCache(GetFibonacci)
 	values := []int{42, 40, 41, 42, 38, 41}
 
+	// Create a wait group
+	var wg sync.WaitGroup
+
 	// For each value to calculate, get the value and print the time it took to calculate
 	for _, v := range values {
-		start := time.Now()
+		// Funtion anonimous to be used in the cache
+		wg.Add(1)
+		go func(v int) {
+			defer wg.Done()
+			start := time.Now()
 
-		value, err := cache.Get(v)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Key %d, Value: %d, time: %s \n", v, value, time.Since(start))
+			value, err := cache.Get(v)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Key %d, Value: %d, time: %s \n", v, value, time.Since(start))
+		}(v)
 	}
+	wg.Wait()
 }
